8b4770df4acac5e9cd3663e328daa1627d9a4e22
Owwwen86 <smirnovkirill@mail.ru>
2023-04-04 21:18:21 +0300

    Lesson 35

--- todolist/core/serializers.py
+++ todolist/core/serializers.py
@@ -1,6 +1,7 @@
+from django.contrib.auth import authenticate
 from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
+from rest_framework.exceptions import ValidationError, AuthenticationFailed
 
 from todolist.core.fields import PasswordField
 from todolist.core.models import User
@@ -23,3 +24,45 @@ class CreateUserSerializer(serializers.ModelSerializer):
         del validated_data['password_repeat']
         validated_data['password'] = make_password(validated_data['password'])
         return super().create(validated_data)
+
+
+class LoginSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(required=True)
+    password = PasswordField()
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'password')
+        read_only_fields = ('id', 'first_name', 'last_name', 'email')
+
+    def create(self, validated_data: dict) -> User:
+        if not (user := authenticate(
+                username=validated_data['username'],
+                password=validated_data['password']
+        )):
+            raise AuthenticationFailed
+        return user
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name', 'last_name', 'email')
+
+
+class UpdatePasswordSerializer(serializers.Serializer):
+    old_password = PasswordField(required=True)
+    new_password = PasswordField(required=True)
+
+    def validate_old_password(self, old_password: str) -> old_password:
+        if not isinstance(self.instance, User):
+            raise NotImplementedError
+        if not self.instance.check_password(old_password):
+            raise ValidationError('Password is incorrect')
+        return old_password
+
+    def update(self, instance: User, validated_data: dict) -> User:
+        instance.set_password(validated_data['new_password'])
+        instance.save(update_fields=('password',))
+        return instance
